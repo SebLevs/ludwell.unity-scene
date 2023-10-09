cb934093df1d6323ecc7f60b06d989277e6c00b0
Sébastien <sebastien.levesque.slev@gmail.com>
2023-10-09 15:53:17 -0400

    feat: setup required scenes for animated drag

--- Runtime/Scripts/Utilities/UiToolkitNames.cs
+++ Runtime/Scripts/Utilities/UiToolkitNames.cs
@@ -5,5 +5,8 @@ namespace Ludwell.Scene
         public static readonly string UnityContentContainerName = "unity-content-container";
         public static readonly string UnityCheckmarkName = "unity-checkmark";
         public static readonly string UnityContentName = "unity-content";
+        public static readonly string ReorderableItemName = "unity-list-view__reorderable-item"; // parent
+        public static readonly string ReorderableHandleName = "unity-list-view__reorderable-handle"; // first child
+        
     }
 }
