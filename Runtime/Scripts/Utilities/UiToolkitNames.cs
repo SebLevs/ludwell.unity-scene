8de20bfc70e290dbe349cfa0674c2b6d1a819606
Sébastien <sebastien.levesque.slev@gmail.com>
2024-03-07 21:53:38 -0500

    chore: remove unused static naming

--- Runtime/Scripts/Utilities/UiToolkitNames.cs
+++ Runtime/Scripts/Utilities/UiToolkitNames.cs
@@ -3,10 +3,6 @@ namespace Ludwell.Scene
     public class UiToolkitNames
     {
         public const string RootVisualContainer = "rootVisualContainer";
-        
-        public const string UnityCheckmark = "unity-checkmark";
-        public const string UnityContent = "unity-content";
-
         public const string UnitySearch = "unity-search";
     }
 }
