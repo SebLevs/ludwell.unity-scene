e521308c3ed525f7804de706227e8379145e42c5
Sébastien <sebastien.levesque.slev@gmail.com>
2024-08-06 19:01:35 -0400

    fix: prevent list and arrays from showing the path-to-guid instead of the element number

--- Editor/Scripts/SceneAssetReference/SceneAssetReferenceDrawer.cs
+++ Editor/Scripts/SceneAssetReference/SceneAssetReferenceDrawer.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine.UIElements;
 
@@ -10,7 +11,17 @@ namespace Ludwell.Scene.Editor
         {
             var serializedProperty = property.FindPropertyRelative("_guid");
             var root = new SceneAssetReferenceController(serializedProperty);
-            root.SetObjectFieldLabel(property.displayName);
+
+            var displayName = property.displayName;
+
+            var match = Regex.Match(property.propertyPath, @"\[(\d+)\]");
+            if (match.Success)
+            {
+                var index = match.Groups[1].Value;
+                displayName = $"Element {index}";
+            }
+
+            root.SetObjectFieldLabel(displayName);
 
             return root;
         }
