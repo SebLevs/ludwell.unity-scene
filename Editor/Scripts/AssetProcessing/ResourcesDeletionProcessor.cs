7f5ff4f181c33f0ed44656440ffcded1b42cba60
Sébastien <sebastien.levesque.slev@gmail.com>
2024-08-13 00:41:16 -0400

    chore: cleanup ResourcesDeletionProcessor

--- Editor/Scripts/AssetProcessing/ResourcesDeletionProcessor.cs
+++ Editor/Scripts/AssetProcessing/ResourcesDeletionProcessor.cs
@@ -8,18 +8,37 @@ namespace Ludwell.Scene.Editor
     {
         private static AssetDeleteResult OnWillDeleteAsset(string assetPath, RemoveAssetOptions options)
         {
+            if (EvaluateAsset(assetPath, out var onWillDeleteAsset)) return onWillDeleteAsset;
+
+            return EvaluateFolder(assetPath);
+        }
+
+        private static bool EvaluateAsset(string assetPath, out AssetDeleteResult onWillDeleteAsset)
+        {
             if (assetPath.EndsWith(".asset"))
             {
                 var fileName = Path.GetFileNameWithoutExtension(assetPath);
                 if (!ResourcesSolver.ScriptableAssets.ContainsKey(fileName))
                 {
-                    return AssetDeleteResult.DidNotDelete;
+                    {
+                        onWillDeleteAsset = AssetDeleteResult.DidNotDelete;
+                        return true;
+                    }
                 }
 
                 Debug.LogWarning($"Suspicious action | Asset is required | {fileName}");
-                return AssetDeleteResult.DidDelete;
+                {
+                    onWillDeleteAsset = AssetDeleteResult.DidDelete;
+                    return true;
+                }
             }
 
+            onWillDeleteAsset = AssetDeleteResult.DidNotDelete;
+            return false;
+        }
+
+        private static AssetDeleteResult EvaluateFolder(string assetPath)
+        {
             if (!Directory.Exists(assetPath)) return AssetDeleteResult.DidNotDelete;
 
             var guids = AssetDatabase.FindAssets($"t:{nameof(ScriptableObject)}", new[] { assetPath });
