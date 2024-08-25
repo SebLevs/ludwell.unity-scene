9be6799bf0338ced00c755d8cdfd2bf06276f290
Sébastien <sebastien.levesque.slev@gmail.com>
2024-08-25 13:58:22 -0400

    feat: add dark themed addressable icons

--- Editor/Scripts/Data/SpritesPath.cs
+++ Editor/Scripts/Data/SpritesPath.cs
@@ -21,7 +21,7 @@ namespace Ludwell.Scene.Editor
         public static readonly string SettingsDark = Path.Combine(SpritesName, "icon_settings-dark");
         public static readonly string AddToBuildSettingsDark = Path.Combine(SpritesName, "icon_add-build-settings-dark");
         public static readonly string EnableInBuildSettingsDark = Path.Combine(SpritesName, "icon_enable-build-settings-dark");
-        public static readonly string AddToAddressablesDark = Path.Combine(SpritesName, "icon_add-addressables");
-        public static readonly string RemoveFromAddressablesDark = Path.Combine(SpritesName, "icon_remove-addressables");
+        public static readonly string AddToAddressablesDark = Path.Combine(SpritesName, "icon_add-addressables-dark");
+        public static readonly string RemoveFromAddressablesDark = Path.Combine(SpritesName, "icon_remove-addressables-dark");
     }
 }
